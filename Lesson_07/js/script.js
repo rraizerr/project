2d2d53e71fa55f789281ec07ec8296f3c8035225
Slava <rraizerr@ukr.net>
2021-07-23 13:21:34 +0300

    sdfssd

---
+++ Lesson_07/js/script.js
@@ -0,0 +1,29 @@
+"use strict";
+
+let nub = 4.6;
+
+console.log(-4 / 0);
+console.log("string" * 9);
+
+const person = "5";
+
+const bool = false;
+
+// console.log(something);
+
+let und;
+console.log(und);
+
+const obj = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(obj.name);
+// console.log(obj["name"]);
+
+let arr = ["plum.png", "orange.jpg", 6, "apple.bmp", [], {}];
+console.log(arr[1]);
+
+console.log(4 * 3 ** 2); // 4 * 9
