fe3830c4545422b233a0ba355370d75e9ebb1f64
Slava <rraizerr@ukr.net>
2021-07-26 16:20:16 +0300

    started lesson N22

--- Lesson_022/js/script.js
+++ Lesson_022/js/script.js
@@ -1,2 +1,23 @@
 "use strict";
 
+// Передача данных по ссылке и по значению
+
+let a = 5,
+    b = a;
+
+b = b + 5;
+
+console.log(b);
+console.log(a);
+
+const obj = {
+    a: 5,
+    b: 1
+};
+
+const copy = obj;
+
+copy.a = 10;
+
+console.log(copy);
+console.log(obj);
