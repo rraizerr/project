2a8e7d2a340f05b385b4c596dc7f058bb9fbf7c5
Slava <rraizerr@ukr.net>
2021-08-23 15:44:12 +0300

    Выполнены некоторые задания на строки

--- LearnJsPractice/script.js
+++ LearnJsPractice/script.js
@@ -843,3 +843,38 @@
 // console.log(randomInteger(1, 5));
 // console.log(randomInteger(1, 5));
 // console.log(randomInteger(1, 5));
+
+// --------------------------------------------------------------------
+// 1)
+// Напишите функцию ucFirst(str), возвращающую строку str с заглавным
+// первым символом.Например:
+
+// ucFirst("вася") == "Вася";
+
+// 1.1)
+// function ucFirst(str) {
+//     let strTwo = str[0].toUpperCase() + str.slice(1);
+    
+//     return strTwo;
+// }
+
+// console.log(ucFirst("вася"));
+
+// 1.2)
+// function ucFirst(str) {
+//     let strTwo = str.charAt(0).toUpperCase() + str.slice(1);
+    
+//     return strTwo;
+// }
+
+// console.log(ucFirst("вася"));
+
+// 1.3)
+// function ucFirst(str) {
+//     if (!str) {
+//         return str;
+//     }
+//     return str[0].toUpperCase() + str.slice(1);
+// }
+
+// console.log(ucFirst("вася"));
