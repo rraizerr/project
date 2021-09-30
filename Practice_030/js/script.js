105dd3a7350b5797ecbb394c105a2eed9536d189
Slava <rraizerr@ukr.net>
2021-10-01 00:48:15 +0300

    Доработана практика 30урока, пройден материал по событиям на мобильных устройствах

--- Practice_030/js/script.js
+++ Practice_030/js/script.js
@@ -14,51 +14,110 @@
 
 'use strict';
 
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
-};
+document.addEventListener("DOMContentLoaded", () => {
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против..."
+        ]
+    };
 
-const // adBlock = document.querySelectorAll(".promo__adv"),
-    adv = document.querySelectorAll(".promo__adv img"),
-    poster = document.querySelector(".promo__bg"),
-    genre = poster.querySelector(".promo__genre"),
-    movieList = document.querySelector(".promo__interactive-list");
+    const // adBlock = document.querySelectorAll(".promo__adv"),
+        adv = document.querySelectorAll(".promo__adv img"),
+        poster = document.querySelector(".promo__bg"),
+        genre = poster.querySelector(".promo__genre"),
+        movieList = document.querySelector(".promo__interactive-list"),
+        addForm = document.querySelector("form.add"),
+        addInput = addForm.querySelector(".adding__input"),
+        checkbox = addForm.querySelector("[type='checkbox']");
+   
+    addForm.addEventListener("submit", function (event) {
+        event.preventDefault();
+        let newFilm = addInput.value;
+        const favorite = checkbox.checked;
+
+        if (newFilm) {
+
+            if (newFilm.length > 21) {
+                newFilm = `${newFilm.substring(0, 22)}...`;
+            }
+
+            if (favorite) {
+                console.log("Добавляем любимый фильм");
+            }
+
+            movieDB.movies.push(newFilm);
+            sortArr(movieDB.movies);
+
+            createMovieList(movieDB.movies, movieList);
+        }
+        
+        event.target.reset(); // Очищаем форму
+    });
+
+    const deleteAdv = (arr) => {
+        arr.forEach(item => {
+            item.remove();
+        });
+    };
+
+    const makeChanges = () => {
+        genre.innerHTML = "драма";
+
+        poster.style.backgroundImage = "url('img/bg.jpg')";
+    };
+
+    const sortArr = (arr) => {
+        arr.sort();
+    };
+
+    function createMovieList(films, parent) {
+        parent.innerHTML = "";
+        sortArr(films);
+        
+        films.forEach((film, i) => {
+            parent.innerHTML += ` 
+                <li class="promo__interactive-item">${i + 1} ${film}
+                    <div class="delete"></div>
+                </li>
+        `;
+        }); document.querySelectorAll(".delete").forEach((btn, i) => {
+            btn.addEventListener("click", () => {
+                btn.parentElement.remove();
+                movieDB.movies.splice(i, 1);
+
+                createMovieList(films, parent);
+            });
+        });
+    }
+
+    deleteAdv(adv);
+    makeChanges();
+    createMovieList(movieDB.movies, movieList);
+
+    // Следующий урок
+    /* 1) Реализовать функционал, что после заполнения формы и нажатия 
+    кнопки "Подтвердить" - 
+    новый фильм добавляется в список. Страница не должна перезагружаться.
+    Новый фильм должен добавляться в movieDB.movies.
+    Для получения доступа к значению input - обращаемся к нему как 
+    input.value;
+    P.S. Здесь есть несколько вариантов решения задачи, принимается любой, 
+    но рабочий.
     
+    2) Если название фильма больше, чем 21 символ - обрезать его и добавить 
+    три точки
+    
+    3) При клике на мусорную корзину - элемент будет удаляться из списка 
+    (сложно)
+    
+    4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести 
+    сообщение: 
+    "Добавляем любимый фильм"
+    
+    5) Фильмы должны быть отсортированы по алфавиту */
 
-
-// while (adBanners.length > 1) {
-//     let i = 1;
-//     adBanners[i].remove();
-// }
-
-adv.forEach(item => {
-    item.remove();
-});
-
-// с помощью обычной функции
-// adv.forEach(function (item) {
-//     item.remove();
-// });
-
-genre.innerHTML = "драма";
-
-// poster.style.cssText = "background-image: url(img/bg.jpg)";
-poster.style.backgroundImage = "url('img/bg.jpg')";
-
-movieList.innerHTML = "";
-
-movieDB.movies.sort();
-
-movieDB.movies.forEach((film, i) => {
-    movieList.innerHTML += ` 
-        <li class="promo__interactive-item">${i + 1} ${film}
-            <div class="delete"></div>
-        </li>
-    `;
 });
---
+++ Practice_034/js/script.js
@@ -0,0 +1,27 @@
+"use strict";
+
+window.addEventListener("DOMContentLoaded", () => {
+    const box = document.querySelector(".box");
+
+    box.addEventListener("touchstart", (e) => {
+        e.preventDefault();
+
+        console.log("Start");
+        console.log(e.touches);
+        console.log(e.targetTouches);
+        console.log(e.changedTouches);
+    });
+
+    box.addEventListener("touchmove", (e) => {
+        e.preventDefault();
+
+        console.log("Move");
+        console.log(e.targetTouches[0].pageX);
+    });
+
+    box.addEventListener("touchend", (e) => {
+        e.preventDefault();
+
+        console.log("End");
+    });
+});
