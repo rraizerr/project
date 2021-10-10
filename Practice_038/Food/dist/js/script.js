d2ee5fa2314bd49c54907bb78668abd70f5edd70
Slava <rraizerr@ukr.net>
2021-10-10 20:32:16 +0300

    Добавлен таймар к проэкту

--- Practice_038/Food/dist/js/script.js
+++ Practice_038/Food/dist/js/script.js
@@ -1,6 +1,7 @@
 "use strict";
 window.addEventListener("DOMContentLoaded", () => {
 
+    // Tabs
     const tabs = document.querySelectorAll(".tabheader__item"),
         tabsContent = document.querySelectorAll(".tabcontent"),
         tabsParent = document.querySelector(".tabheader__items");
@@ -16,7 +17,7 @@ window.addEventListener("DOMContentLoaded", () => {
         });
     }
 
-    function showTabContent(i = 0) { // указано дефолтное значение 1 элемент
+    function showTabContent(i = 0) {
         tabsContent[i].classList.add("show", "fade");
         tabsContent[i].classList.remove("hide");
         tabs[i].classList.add("tabheader__item_active");
@@ -24,8 +25,7 @@ window.addEventListener("DOMContentLoaded", () => {
     }
     
     hideTabContent();
-    showTabContent(); // showTabContent(0); передаем первый элемент для 
-    //показа(первый слайд)
+    showTabContent();
 
     tabsParent.addEventListener("click", (event) => {
         const target = event.target;
@@ -39,4 +39,57 @@ window.addEventListener("DOMContentLoaded", () => {
             });
         }
     });
+
+    // Timer
+    const deadLine = "2021-10-15";
+
+    function getTimeRemaining(endtime) {
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+            days = Math.floor(t / (1000 * 60 * 60 * 24)),
+            hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            seconds = Math.floor((t / 1000) % 60);
+        
+        return {
+            "total": t,
+            "days": days,
+            "hours": hours,
+            "minutes": minutes,
+            "seconds": seconds
+        };
+    }
+
+    function getZero(num) {
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    }
+
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector), // Передаем наш таймер: класс .timer
+            days = timer.querySelector("#days"),
+            hours = timer.querySelector("#hours"),
+            minutes = timer.querySelector("#minutes"),
+            seconds = timer.querySelector("#seconds"),
+            timeInterval = setInterval(updateClock, 1000);
+
+        updateClock();
+        
+        function updateClock() {
+            const t = getTimeRemaining(endtime);
+
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setClock(".timer", deadLine);
 });
