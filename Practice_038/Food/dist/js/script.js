5b2f68e8a6fa3250194bd1086d2cf6f2b426707e
Slava <rraizerr@ukr.net>
2021-10-15 23:35:30 +0300

    Создание модального окна

--- Practice_038/Food/dist/js/script.js
+++ Practice_038/Food/dist/js/script.js
@@ -41,7 +41,7 @@ window.addEventListener("DOMContentLoaded", () => {
     });
 
     // Timer
-    const deadLine = "2021-10-15";
+    const deadLine = "2021-10-30";
 
     function getTimeRemaining(endtime) {
         const t = Date.parse(endtime) - Date.parse(new Date()),
@@ -92,4 +92,39 @@ window.addEventListener("DOMContentLoaded", () => {
     }
 
     setClock(".timer", deadLine);
+
+    // Modal
+
+    const modalTrigger = document.querySelectorAll("[data-modal]"),
+        modal = document.querySelector(".modal"),
+        modalCloseBtn = document.querySelector("[data-close]");
+
+    modalTrigger.forEach(btn => {
+        btn.addEventListener("click", () => {
+            modal.classList.add("show");
+            modal.classList.remove("hide");
+            document.body.style.overflow = "hidden";
+        });
+    });
+    
+    function closeModal() {
+        modal.classList.add("hide");
+        modal.classList.remove("show");
+        document.body.style.overflow = "";
+    }
+
+
+    modalCloseBtn.addEventListener("click", closeModal);
+
+    modal.addEventListener("click", (e) => {
+        if (e.target === modal ) {
+            closeModal();
+        }
+    });
+
+    document.addEventListener("keydown", (e) => {
+        if (e.code === "Escape" && modal.classList.contains("show")) {
+            closeModal();
+        }
+    });
 });
