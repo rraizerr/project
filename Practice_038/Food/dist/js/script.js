4ad99411f167964e500b4109fef59ff2e514302a
Slava <rraizerr@ukr.net>
2021-10-23 22:42:37 +0300

    Урок 53. Реализация скрипта отправки данных на ссервер

--- Practice_038/Food/dist/js/script.js
+++ Practice_038/Food/dist/js/script.js
@@ -166,7 +166,7 @@ window.addEventListener("DOMContentLoaded", () => {
             // Проверяем, передан ли параметр класса, если нет ,используем умолчание
             if (this.classes.length === 0) {
                 this.element = "menu__item"; // Параметр по умолчанию
-                element.classList.add(this.element)
+                element.classList.add(this.element);
             } else { // Если параметры переданы
                this.classes.forEach(className => element.classList.add(className));
             }
@@ -211,4 +211,78 @@ window.addEventListener("DOMContentLoaded", () => {
         21,
         ".menu .container"
     ).render();
+
+    // Forms
+
+    const forms = document.querySelectorAll("form");
+
+    const message = {
+        loading: "Загрузка",
+        success: "Спасибо! Скоро мы с Вами свяжемся",
+        failure: "Что-то пошло не так..."
+    };
+
+    forms.forEach(item => {
+        postData(item);
+    });
+
+    function postData(form) {
+        form.addEventListener("submit", (e) => {
+            e.preventDefault();
+
+            const statusMessage = document.createElement("div");
+            statusMessage.classList.add("status");
+            statusMessage.textContent = message.loading;
+            form.append(statusMessage);
+
+            const request = new XMLHttpRequest();
+            request.open("POST", "server.php");
+
+            // request.setRequestHeader("Content-type", "multipart/form-data"); 
+            //Заголовок устанавливать не нужно, он поставится автоматически
+            // const formData = new FormData(form);
+
+            // request.send(formData);
+
+            // request.addEventListener("load", () => {
+            //     if (request.status === 200) {
+            //         console.log(request.response);
+            //         statusMessage.textContent = message.success;
+            //         form.reset(); // Очищаем форму
+            //         setTimeout(() => {
+            //             statusMessage.remove()
+            //         }, 2000);
+            //     } else {
+            //         statusMessage.textContent = message.failure;
+            //     }
+            // });
+
+            // Отправлляем форму в формате JSON
+            request.setRequestHeader("Content-type", "application/json"); 
+            const formData = new FormData(form);
+
+            const object = {};
+            formData.forEach(function (value, key) {
+                object[key] = value;
+            });
+
+            const json = JSON.stringify(object);
+
+            request.send(json);
+
+            request.addEventListener("load", () => {
+                if (request.status === 200) {
+                    console.log(request.response);
+                    statusMessage.textContent = message.success;
+                    form.reset(); // Очищаем форму
+                    setTimeout(() => {
+                        statusMessage.remove()
+                    }, 2000);
+                } else {
+                    statusMessage.textContent = message.failure;
+                }
+            });
+
+        });
+    }
 });
