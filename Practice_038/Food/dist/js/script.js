10806814afdcd24a74c568b5b8668b7c0fdd513f
Slava <rraizerr@ukr.net>
2021-11-08 22:23:11 +0200

    В слайдере заменил метод slice на replace используя regExp и вынес его в отдельную функцию, закомментировл код, который не используется

--- Practice_038/Food/dist/js/script.js
+++ Practice_038/Food/dist/js/script.js
@@ -187,15 +187,15 @@ window.addEventListener("DOMContentLoaded", () => {
         }
     }
     
-    const getResorce = async (url) => {
-        const res = await fetch(url);
+    // const getResorce = async (url) => {
+    //     const res = await fetch(url);
         
-        if (!res.ok) { // если в запросе пошло что-то не так, в консоль выкидываем ошибку
-            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
-        }
+    //     if (!res.ok) { // если в запросе пошло что-то не так, в консоль выкидываем ошибку
+    //         throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+    //     }
 
-        return await res.json();
-    };
+    //     return await res.json();
+    // };
 
     // //при помощи сервера запроса мы получаем массив, который содержит menu
     // //это массив с объектами. Перебераем этот массив методом forEach
@@ -448,12 +448,16 @@ window.addEventListener("DOMContentLoaded", () => {
         dots[slideIndex - 1].style.opacity = 1;
     }
 
+    function deleteNotDigits (str) {
+        return +str.replace(/\D/g, ""); // ищем все не цифры \D, глобально \g и заменяем на пустое место ""
+    }
+
     next.addEventListener("click", () => {
-        if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+        if (offset == deleteNotDigits(width) * (slides.length - 1)) {
                       // Удаляем "px" от размера слайда (500px/ станет 500) 
             offset = 0;
         } else {
-            offset += +width.slice(0, width.length - 2);
+            offset += deleteNotDigits(width);
         }
 
         slidesField.style.transform = `translateX(-${offset}px)`;
@@ -471,9 +475,9 @@ window.addEventListener("DOMContentLoaded", () => {
     prev.addEventListener("click", () => {
         if (offset == 0) {
                       // Удаляем "px" от размера слайда (500px/ станет 500) 
-            offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+            offset = deleteNotDigits(width) * (slides.length - 1);
         } else {
-            offset -= +width.slice(0, width.length - 2);
+            offset -= deleteNotDigits(width);
         }
 
         slidesField.style.transform = `translateX(-${offset}px)`;
@@ -494,7 +498,7 @@ window.addEventListener("DOMContentLoaded", () => {
 
             // Устанавливаем индекс слайдов в позицию slideTo
             slideIndex = slideTo;
-            offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+            offset = deleteNotDigits(width) * (slideTo - 1);
 
             slidesField.style.transform = `translateX(-${offset}px)`;
 
