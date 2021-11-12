29e38757b864eb9b092445c952afedf2e8b60982
Slava <rraizerr@ukr.net>
2021-11-12 21:21:49 +0200

    Урок улучшен калькулятор, добалнено хранение данных в локальное хранилище

--- Practice_038/Food/dist/js/script.js
+++ Practice_038/Food/dist/js/script.js
@@ -510,9 +510,40 @@ window.addEventListener("DOMContentLoaded", () => {
     // Calc
 
     const result = document.querySelector(".calculating__result span");
-    let sex = "female",
-        height, weight, age,
+    let sex, height, weight, age,
         ratio = 1.375;
+
+    if (localStorage.getItem("sex")) {
+        sex = localStorage.getItem("sex");
+    } else {
+        sex = "female";
+        localStorage.setItem("sex", "female");
+    }
+    
+    if (localStorage.getItem("ratio")) {
+        ratio = localStorage.getItem("ratio");
+    } else {
+        ratio = 1.375;
+        localStorage.setItem("ratio", 1.375);
+    }
+
+    // Функция дает класс активности кномкам калькулятора исходя из данных из localStorage
+    function initLocalSettings(selector, activeClass) {
+        const elements = document.querySelectorAll(selector);
+
+        elements.forEach(elem => {
+            elem.classList.remove(activeClass);
+            if (elem.getAttribute("id") === localStorage.getItem("sex")) {
+                elem.classList.add(activeClass);
+            }
+            if (elem.getAttribute("data-ratio") === localStorage.getItem("ratio")) {
+                elem.classList.add(activeClass);
+            }
+        });
+    }
+
+    initLocalSettings("#gender div", "calculating__choose-item_active");
+    initLocalSettings(".calculating__choose_big div", "calculating__choose-item_active");
 
     function calcTotal() {
         if (!sex || !height || !weight || !age || !ratio) {
@@ -530,17 +561,20 @@ window.addEventListener("DOMContentLoaded", () => {
 
     calcTotal();
 
-    function getStaticInformation(parentSelector, activeClass) {
+    function getStaticInformation(selector, activeClass) {
         //передаем parentSelector в качестве аргумента и получаем все div внутри него
-        const elements = document.querySelectorAll(`${parentSelector} div`);
+        const elements = document.querySelectorAll(selector);
 
         elements.forEach(elem => {
             elem.addEventListener("click", (e) => {
                 //если у объекта событие есть аттрибут, присваиваем его ratio
                 if (e.target.getAttribute("data-ratio")) {
                     ratio = +e.target.getAttribute("data-ratio");
+                    //записываем выбор пользывателя в локальное хранилище
+                    localStorage.setItem("ratio", +e.target.getAttribute("data-ratio"));
                 } else {
                     sex = e.target.getAttribute("id");
+                    localStorage.setItem("sex", e.target.getAttribute("id"));
                 }
 
                 elements.forEach(elem => {
@@ -554,13 +588,21 @@ window.addEventListener("DOMContentLoaded", () => {
         });
     }
 
-    getStaticInformation("#gender", "calculating__choose-item_active");
-    getStaticInformation(".calculating__choose_big", "calculating__choose-item_active");
+    // т.к. обращамся к блока внутри селекторов gender и .calculating__choose_big, нужно добавить dix
+    getStaticInformation("#gender div", "calculating__choose-item_active");
+    getStaticInformation(".calculating__choose_big div", "calculating__choose-item_active");
 
     function getDynamicInformation(selector) {
         const input = document.querySelector(selector);
 
         input.addEventListener("input", () => {
+
+            if (input.value.match(/\D/g)) {
+                input.style.border = "1px solid red";
+            } else {
+                input.style.border = "none";
+            }
+            
             switch (input.getAttribute("id")) {
                 case "height":
                     height = +input.value;
