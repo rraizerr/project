bf8af4d45b1835331048029679cfc733e8256670
Slava <rraizerr@ukr.net>
2022-01-02 18:23:49 +0200

    Урок 69. Инкапсуляция

---
+++ Lesson_069/script.js
@@ -0,0 +1,40 @@
+'use strict';
+
+class User {
+    constructor(name, age) {
+        this.name = name;
+        this._age = age;
+    }
+
+    #surname = 'Petrychenko';
+
+    get surname() {
+        return this.#surname;
+    }
+
+    set surname(surname) {
+        this.#surname = surname;
+    }
+
+    say = () => {
+        console.log(`Имя пользователя: ${this.name} ${this.#surname}, возраст ${this._age}`);
+    }
+
+    get age() {
+        return this._age;
+    }
+
+    set age(age) {
+        if (typeof age === 'number' && age > 0 && age < 110) {
+            this._age = age;
+        } else {
+            console.log('Недопустимое значение!');
+        }
+    }
+}
+
+const ivan = new User('Ivan', 27);
+console.log(ivan.surname);
+ivan.surname = "Night";
+console.log(ivan.surname);
+ivan.say();
