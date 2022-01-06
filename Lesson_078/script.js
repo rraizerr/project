3ec61a1086c7da6bf1c386f145245e8eadc2f15b
Slava <rraizerr@ukr.net>
2022-01-06 11:35:44 +0200

    Функции-генераторы. Урок 78

---
+++ Lesson_078/script.js
@@ -0,0 +1,36 @@
+'use strict';
+
+function* generator() {
+    yield "S";
+    yield "c";
+    yield "r";
+    yield "i";
+    yield "p";
+    yield "t";
+}
+
+const str = generator();
+// console.log(str.next());
+// console.log(str.next());
+// console.log(str.next());
+// console.log(str.next());
+// console.log(str.next());
+// console.log(str.next());
+// console.log(str.next());
+console.log(str.next().value);
+
+function* count(n) {
+    for (let i = 0; i < n; i++) {
+        yield i;
+    }
+}
+
+// const counter = count(7);
+// console.log(counter.next().value);
+// console.log(counter.next().value);
+// console.log(counter.next().value);
+// console.log(counter.next().value);
+
+for (let k of count(7)) {
+    console.log(k);
+}
