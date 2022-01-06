4b19b7d514f5b390cdafe066747c6715c2637f2e
Slava <rraizerr@ukr.net>
2022-01-06 15:30:06 +0200

    Движение анимации после нажатия кнопки, в четырех направлениях

---
+++ Lesson_079/script.js
@@ -0,0 +1,52 @@
+const btnTop = document.querySelector('.top'),
+    btnLeft = document.querySelector('.left'),
+    btnRight = document.querySelector('.right'),
+    btnDown = document.querySelector('.down'),
+    elem = document.querySelector('.box');  
+let pos = [0, 0];
+
+function myAnimationRight() {
+    pos[0]++;
+    elem.style.left = pos[0] + "px";
+
+    if (pos[0] < 300) {
+        requestAnimationFrame(myAnimationRight);
+        console.log(pos);
+    }
+}
+
+function myAnimationLeft() {
+    pos[0]--;
+    elem.style.left = pos[0] + "px";
+
+    if (pos[0] > 0) {
+        requestAnimationFrame(myAnimationLeft);
+        console.log(pos);
+    }
+}
+
+function myAnimationDown() {
+    pos[1]++;
+    elem.style.top = pos[1] + "px";
+
+    if (pos[1] < 300) {
+        requestAnimationFrame(myAnimationDown);
+        console.log(pos);
+    }
+}
+
+function myAnimationTop() {
+    pos[1]--;
+    elem.style.top = pos[1] + "px";
+
+    if (pos[1] > 0) {
+        requestAnimationFrame(myAnimationTop);
+        console.log(pos);
+    }
+}
+
+btnRight.addEventListener('click', () => requestAnimationFrame(myAnimationRight));
+btnDown.addEventListener('click', () => requestAnimationFrame(myAnimationDown));
+
+btnLeft.addEventListener('click', () => requestAnimationFrame(myAnimationLeft));
+btnTop.addEventListener('click', () => requestAnimationFrame(myAnimationTop));
