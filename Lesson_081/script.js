627fc01ae862e1564cd0ef8ca92ac2479ca8868d
Slava <rraizerr@ukr.net>
2022-01-09 12:18:26 +0200

    Разбор работы синхронных и асинхронных операций. Урок 81

---
+++ Lesson_081/script.js
@@ -0,0 +1,32 @@
+"use strict";
+
+// console.log(1);
+
+// setTimeout(() => {
+//     console.log("timeout");
+// }, 2000);
+
+// setTimeout(() => {
+//     console.log("timeout_4000");
+// }, 4000);
+
+// console.log(2);
+
+// -------------------------
+// let k = 0;
+
+// function coutn() {
+//     for (let i = 0; i < 1e9; i++) {
+//         k++;
+//     }
+//     alert("Done");
+// }
+
+// coutn();
+
+// -------------------------
+setTimeout(() => {
+    console.log(1);
+}, 0);
+
+console.log(2);
