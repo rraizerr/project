5552045a908e74fbb45e01ca307ba1a0aa3cd494
Slava <rraizerr@ukr.net>
2022-01-10 17:32:00 +0200

    Реакт, основные понятия, установка и первая работа. Урок 84

---
+++ React_084/my-app/src/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+// 1.
+// const elem = <h2>Hello World!</h2>;
+
+// 2.
+// const elem = React.createElement("h2", null, "Hello World!");
+// второй аргумент - это название класса. У нас его нет по этому null
+
+// 3.
+// const elem = React.createElement("h2", {className: "greetings"}, "Hello World!");
+
+// 4.
+// const elem = (
+//   <div>
+//     <h2>Hello World!</h2>
+//     <input type="text" />
+//     <button>Click</button>
+//   </div>
+// );
+
+// 5.
+const text = "Hello World!";
+
+const elem = (
+  <div>
+    <h2 className="text">Текст: {text}</h2>
+    <input type="text" />
+    <label htmlFor=""></label>
+    <button tabIndex="0">Click</button>
+  </div>
+);
+
+ReactDOM.render(
+  elem,
+  document.getElementById('root')
+);
