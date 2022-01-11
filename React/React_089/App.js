5780afac5c096ef4528cf758738e4d99b504cef4
Slava <rraizerr@ukr.net>
2022-01-11 11:55:40 +0200

    Strict Mode, Строгий режим

---
+++ React/React_089/App.js
@@ -0,0 +1,56 @@
+// 1. import React from 'react';
+import {Component, StrictMode} from 'react';
+import './App.css';
+
+const Header = () => {
+  return <h2>Hello World!</h2>
+}
+
+// const Field = () => {
+//   const holder = "Enter here";
+//   const styledField = {
+//     width: "300px"
+//   };
+//   return <input
+//           placeholder={holder}
+//           type="text"
+//           style={styledField} />;
+// }
+
+// 1. class Field extends React.Component {
+
+// }
+
+class Field extends Component {
+  render() {
+    const holder = "Enter here";
+    const styledField = {
+    width: "300px"
+    };
+    
+    return <input
+            placeholder={holder}
+            type="text"
+            style={styledField} />;
+  }
+}
+
+function Btn() {
+  const text = "Log in";
+  const logged = true;
+  return <button>{logged ? "Enter" : text}</button>
+}
+
+function App() {
+  return (
+    <div className="App">
+      <StrictMode>
+        <Header />
+      </StrictMode>
+      <Field />
+      <Btn/>
+    </div>
+  );
+}
+
+export default App;
