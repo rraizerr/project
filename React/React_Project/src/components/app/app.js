9730e6173dbaa28f05aee0d1dadb7ff604317ddc
Slava <rraizerr@ukr.net>
2022-01-11 18:38:20 +0200

    Работа со свойствами

--- React/React_Project/src/components/app/app.js
+++ React/React_Project/src/components/app/app.js
@@ -7,6 +7,13 @@ import EmployeersAddForm from "../employeers-add-form/employeers-add-form";
 import "./app.css";
 
 function App() {
+
+    const data = [
+        { name: "John C.", salary: 800, increase: false},
+        { name: "Alex M.", salary: 3000, increase: true},
+        { name: "Carl W.", salary: 5000, increase: false}
+    ];
+
     return (
         <div className="app">
             <AppInfo />
@@ -16,7 +23,7 @@ function App() {
                 <AppFilter />
             </div>
 
-            <EmployeersList />
+            <EmployeersList data={data}/>
             <EmployeersAddForm />
         </div>
     );
--- React/React_Project/src/components/employeers-list/employeers-list.js
+++ React/React_Project/src/components/employeers-list/employeers-list.js
@@ -2,12 +2,18 @@ import EmployeersListItem from "../employeers-list-item/employeers-list-item";
 
 import "./employeers-list.css"
 
-const EmployeersList = () => {
+const EmployeersList = ({data}) => {
+    
+    const elements = data.map(item => {
+        return (
+            <EmployeersListItem {...item} />
+            // name={item.name} salary={item.salary} идентична этой записи {...item}
+        )
+    })
+
     return (
         <ul className="app-list list-group">
-            <EmployeersListItem />
-            <EmployeersListItem />
-            <EmployeersListItem />
+            {elements}
         </ul>
     );
 }
