51b2cfbb8f1088f0b131936b4e28e08cea3ff76f
Slava <rraizerr@ukr.net>
2022-01-12 11:30:51 +0200

    состояние компонентов

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -1,23 +1,107 @@
-import logo from './logo.svg';
+import { Component } from 'react';
+
 import './App.css';
+
+// ----------------- 1. Передаем значения
+// function WhoAmI(props) {
+//   return (
+//     <div>
+//       <h1>My name is {props.name}, surname - {props.surname}</h1>
+//       <a href={props.link}>My profile</a>
+//     </div>
+//   );
+// }
+
+// ----------------- 2. Передаем значения
+// function WhoAmI({name, surname, link}) {
+//   return (
+//     <div>
+//       <h1>My name is {name}, surname - {surname}</h1>
+//       <a href={link}>My profile</a>
+//     </div>
+//   );
+// }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <WhoAmI name="John" surname="Smith" link="facebook.com" />
+//       <WhoAmI name="Alex" surname="Shepard" link="vk.com"/>
+//     </div>
+//   );
+// }
+
+// -----------------Передаем как объект
+// function WhoAmI({name, surname, link}) {
+//   return (
+//     <div>
+//       <h1>My name is {name.firstName}, surname - {surname}</h1>
+//       <a href={link}>My profile</a>
+//     </div>
+//   );
+// }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <WhoAmI name={{firstName: "John"}} surname="Smith" link="facebook.com" />
+//       <WhoAmI name={{firstName: "Alex"}} surname="Shepard" link="vk.com"/>
+//     </div>
+//   );
+// }
+
+// -----------------Передаем как функцию
+// function WhoAmI({name, surname, link}) {
+//   return (
+//     <div>
+//       <h1>My name is {name()}, surname - {surname}</h1>
+//       <a href={link}>My profile</a>
+//     </div>
+//   );
+// }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <WhoAmI name={() => {return "John"}} surname="Smith" link="facebook.com" />
+//       <WhoAmI name={() => {return "Alex"}} surname="Shepard" link="vk.com"/>
+//     </div>
+//   );
+// }
+
+//--------------------Используем класс
+class WhoAmI extends Component  {
+  constructor(props) {
+    super(props);
+    this.state = {
+      years: 27,
+      text: "+++"
+    }
+  }
+
+  nextYear = () => {
+    this.setState(state => ({ // этот метод используется для изменения состояния
+      years: state.years + 1  // callback используется для точности и последовательности данных
+    }))
+  }
+
+  render() {
+    const { name, surname, link } = this.props;
+    return(
+      <div>
+        <button onClick={this.nextYear}>{this.state.text}</button>  
+        <h1>My name is {name}, surname - {surname}, age - {this.state.years}</h1>
+        <a href={link}>My profile</a>
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <WhoAmI name="John" surname="Smith" link="facebook.com" />
+      <WhoAmI name="Alex" surname="Shepard" link="vk.com"/>
     </div>
   );
 }
--- React/my-app/src/index.js
+++ React/my-app/src/index.js
@@ -1,59 +1,11 @@
-import React from 'react';
+import React, {StrictMode} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
-function formatDate(date) {
-  return date.toLocaleDateString();
-}
-
-function Avatar(props) {
-  return (
-    <img className="Avatar"
-      src={props.user.avatarUrl}
-      alt={props.user.name}
-    />
-  );
-}
-
-function UserInfo(props) {
-  return (
-    <div className="UserInfo">
-      <Avatar user={props.user} />
-      <div className="UserInfo-name">
-        {props.user.name}
-      </div>
-    </div>
-  );
-}
-
-function Comment(props) {
-  return (
-    <div className="Comment">
-      <UserInfo user={props.author} />
-      <div className="Comment-text">
-        {props.text}
-      </div>
-      <div className="Comment-date">
-        {formatDate(props.date)}
-      </div>
-    </div>
-  );
-}
-
-const comment = {
-  date: new Date(),
-  text: 'I hope you enjoy learning React!',
-  author: {
-    name: 'Hello Kitty',
-    avatarUrl: 'https://placekitten.com/g/64/64',
-  },
-};
+import App from './App';
 
 ReactDOM.render(
-  <Comment
-    date={comment.date}
-    text={comment.text}
-    author={comment.author}
-  />,
+  <StrictMode>
+    <App />
+  </StrictMode>,
   document.getElementById('root')
 );
