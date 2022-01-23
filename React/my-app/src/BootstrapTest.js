f1d835fec9df96df27eec96886c41e2723c8154e
Slava <rraizerr@ukr.net>
2022-01-23 18:55:06 +0200

    Готовые стили для проэкта. React bootstrap. Приминение на практике

---
+++ React/my-app/src/BootstrapTest.js
@@ -0,0 +1,73 @@
+import { Container, Row, Col, Carousel, Form, Button } from "react-bootstrap";
+
+const BootstrapTest = () => {
+    return (
+        <Container className="mt-5 mb-5">
+            <Row>
+                <Col>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" />
+                            <Form.Text className="text-muted">
+                            We'll never share your email with anyone else.
+                            </Form.Text>
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                            <Form.Check type="checkbox" label="Check me out" />
+                        </Form.Group>
+                        <Button variant="primary" type="submit">
+                            Submit
+                        </Button>
+                    </Form>
+                </Col>
+                <Col>
+                    <Carousel>
+                        <Carousel.Item>
+                            <img
+                            className="d-block w-100"
+                            src="https://ichef.bbci.co.uk/news/976/cpsprodpb/6D5A/production/_119449972_10.jpg"
+                            alt="First slide"
+                            />
+                            <Carousel.Caption>
+                            <h3>First slide label</h3>
+                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                            className="d-block w-100"
+                            src="https://d3rf6j5nx5r04a.cloudfront.net/NbpnSS19UD0HnElVoaRmn1dS_LQ=/2000x1000/product/6/1/97a47a9c1f354a448024c521e5f7cab2.jpg"
+                            alt="Second slide"
+                            />
+
+                            <Carousel.Caption>
+                            <h3>Second slide label</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                            className="d-block w-100"
+                            src="https://photographypla.net/wp-content/uploads/2013/10/PP1.jpg"
+                            alt="Third slide"
+                            />
+
+                            <Carousel.Caption>
+                            <h3>Third slide label</h3>
+                            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                    </Carousel>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default BootstrapTest;
--- React/my-app/src/index.js
+++ React/my-app/src/index.js
@@ -2,8 +2,11 @@ import React, {StrictMode} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import styled from "styled-components";
 import { Button } from "./App";
+import BootstrapTest from './BootstrapTest';
+import styled from "styled-components";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const BigButton = styled(Button)`
   margin: 0 auto;
@@ -15,6 +18,7 @@ ReactDOM.render(
   <StrictMode>
     <App />
     <BigButton as="a">Отправить отчет</BigButton>
+    <BootstrapTest />
   </StrictMode>,
   document.getElementById('root')
 );
