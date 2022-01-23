cb5f8f86300534bd814e9f348d928d129bd99bc5
Slava <rraizerr@ukr.net>
2022-01-23 19:08:05 +0200

    Поля классов, статические методы

--- React/React_Project/src/components/employees-add-form/employees-add-form.js
+++ React/React_Project/src/components/employees-add-form/employees-add-form.js
@@ -4,12 +4,10 @@ import { Component } from "react";
 import "./employees-add-form.scss";
 
 class EmployeesAddForm extends Component {
-    constructor (props) {
-        super(props);
-        this.state = {
-            name: "",
-            salary: "" 
-        }
+
+    state = {
+        name: "",
+        salary: "" 
     }
 
     onValueChange = (e) => {
@@ -28,6 +26,12 @@ class EmployeesAddForm extends Component {
             salary: "" 
         })
     }
+
+    static onLog = () => {
+        console.log("Hey");
+    }
+
+    static logged = "on";
 
     render() {
         const { name, salary } = this.state;
@@ -57,5 +61,8 @@ class EmployeesAddForm extends Component {
         );
     }
 }
+
+EmployeesAddForm.onLog();
+console.log(EmployeesAddForm.logged);
 
 export default EmployeesAddForm;
