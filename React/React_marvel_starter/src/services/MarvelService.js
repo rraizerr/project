7e5f583ae5bc3d8f21f0361a968ed35b239e83c6
Slava <rraizerr@ukr.net>
2022-02-07 14:21:46 +0200

    Работа с сервером. Трансформация данных и создание компонента со случайным персонажем

--- React/React_marvel_starter/src/services/MarvelService.js
+++ React/React_marvel_starter/src/services/MarvelService.js
@@ -1,6 +1,6 @@
-
-
 class MarvelService {
+    _apiBase = "https://gateway.marvel.com:443/v1/public/";
+    _apiKey = "apikey=33b106e26ee93ee332a38915700a4d3b";
     getResource = async (url) => {
         const res = await fetch(url);
 
@@ -11,9 +11,24 @@ class MarvelService {
         return await res.json();
     }
 
-    getAllCharecters = () => {
-        return this.getResource(`https://gateway.marvel.com:443/v1/public/characters?
-        apikey=33b106e26ee93ee332a38915700a4d3b`);
+    getAllCharecters = async () => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter);
+    }
+
+    getCharacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
+    }
+
+    _transformCharacter = (char) => {
+        return {
+            name: char.name,
+            description: char.description ? `${char.description.slice(0, 210)}...` : "There is no description for this character",
+            thumbnail: char.thumbnail.path + "." + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url
+        }
     }
 }
 export default MarvelService;
